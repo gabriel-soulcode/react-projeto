d747b182d83d7a01003cdb44dc3634b27fc52dee
gab-braga <gabrielbraga0712@gmail.com>
2025-02-03 17:23:13 -0300

    aula 18

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Header></Header>
+      <h1>Oi</h1>
+      <h2>Olá</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Mercado Code</h1>
+    </header>
+  )
+}
+
+// para gerar o componente use o snippet "rfc"
