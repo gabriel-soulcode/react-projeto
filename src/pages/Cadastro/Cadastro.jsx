cb4ecdbcc3f2f9838ffa1db86dc6342551e2d71e
gab-braga <gabrielbraga0712@gmail.com>
2025-02-07 15:09:56 -0300

    aula 22

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 export default function Cadastro() {
@@ -12,15 +12,37 @@ export default function Cadastro() {
     }, 3000);
   }
 
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [inativo, setInativo] = useState(true);
+
+  useEffect(() => {
+    if (nome.length == 0 || email.length == 0) {
+      setInativo(true);
+    } else {
+      setInativo(false);
+    }
+  }, [nome, email]);
+
   return (
     <div>
       <h1>Cadastro</h1>
       <Link to="/">Home</Link>
 
       <br /><br />
-      <input type="text" placeholder="Digite seu nome" />
+      <p>{nome} - {email}</p>
 
-      <button onClick={cadastrar}>
+      <input type="text" placeholder="Digite seu nome" onChange={event => {
+        const texto = event.target.value;
+        setNome(texto);
+      }} /> <br />
+
+      <input type="text" placeholder="Digite seu email" onChange={event => {
+        const texto = event.target.value;
+        setEmail(texto);
+      }} /> <br />
+
+      <button onClick={cadastrar} disabled={inativo}>
         Cadastrar
       </button>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
@@ -8,6 +9,9 @@ export default function Home() {
       <Link to="/cadastro">Cadastro</Link>
       <br />
       <Link to="/estoque">Estoque</Link>
+      <br />
+
+      <Button variant="danger">Saiba mais</Button>
     </div>
   );
 }
