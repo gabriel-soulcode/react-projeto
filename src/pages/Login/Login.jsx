f706c80a925b35e10c436e610f0d4bfd4054c189
gab-braga <gabrielbraga0712@gmail.com>
2025-02-11 13:31:28 -0300

    atividade aula 22

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,8 +3,8 @@ import { Button, Container, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 export default function Login() {
-  const { handleSubmit, register } = useForm();
-  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  const { handleSubmit, register, formState: { errors } } = useForm();
+  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   function enviarLogin(dados) {
     console.log(dados);
@@ -20,7 +20,15 @@ export default function Login() {
             Email
             <span className="text-danger">*</span>
           </Form.Label>
-          <Form.Control {...register("email", { required: true, pattern: regexEmail })} type="email" placeholder="name@example.com" />
+          <Form.Control
+            {...register("email", {
+              required: { value: true, message: "Preencha com seu email." },
+              pattern: { value: regexEmail, message: "Email inválido." },
+            })}
+            type="email"
+            placeholder="name@example.com"
+          />
+          <small className="text-danger">{errors.email?.message}</small>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="senha">
@@ -28,7 +36,16 @@ export default function Login() {
             Senha
             <span className="text-danger">*</span>
           </Form.Label>
-          <Form.Control {...register("senha", { required: true })} type="password" placeholder="Digite sua senha" />
+          <Form.Control
+            {...register("senha", {
+              required: { value: true, message: "Preencha com sua senha." },
+              maxLength: { value: 20, message: "Máximo de 20 caracteres." },
+              minLength: { value: 6, message: "Minimo de 6 caracteres." }
+            })}
+            type="password"
+            placeholder="Digite sua senha"
+          />
+          <small className="text-danger">{errors.senha?.message}</small>
         </Form.Group>
 
         <Button type="submit">Entrar</Button>
