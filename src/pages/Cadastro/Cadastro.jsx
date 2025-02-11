14c05e7ec2649a20fafd5cc969a13b3777177ce9
gab-braga <gabrielbraga0712@gmail.com>
2025-02-11 10:29:55 -0300

    atividade aula 23

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,22 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 export default function Cadastro() {
-  
-  function cadastrar() { }
+  const { handleSubmit, register, formState } = useForm();
+  const { errors } = formState;
+  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  function cadastrar(dados) {
+    console.log(dados);
+  }
 
   return (
-    <div>
+    <Container>
       <h1>Cadastro</h1>
       <Link to="/">Home</Link>
 
-      <input type="text" placeholder="Digite seu nome" />
-      <br />
+      <Form onSubmit={handleSubmit(cadastrar)} className="mt-4">
+        <Form.Group className="mb-3" controlId="nome">
+          <Form.Label>
+            Nome
+            <span className="text-danger">*</span>
+          </Form.Label>
+          <Form.Control
+            {...register("nome", {
+              required: { value: true, message: "Preencha com seu nome." },
+            })}
+            type="text"
+            placeholder="Digite seu nome"
+          />
+          <small className="text-danger">{errors.nome?.message}</small>
+        </Form.Group>
 
-      <input type="text" placeholder="Digite seu email" />
-      <br />
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>
+            Email
+            <span className="text-danger">*</span>
+          </Form.Label>
+          <Form.Control
+            {...register("email", {
+              required: { value: true, message: "Preencha com seu email" },
+              pattern: { value: regexEmail, message: "E-mail inválido." },
+            })}
+            type="email"
+            placeholder="Digite seu email"
+          />
+          <small className="text-danger">{errors.email?.message}</small>
+        </Form.Group>
 
-      <button>Cadastrar</button>
-    </div>
+        <Form.Group className="mb-3" controlId="senha">
+          <Form.Label>
+            Senha
+            <span className="text-danger">*</span>
+          </Form.Label>
+          <Form.Control
+            {...register("senha", {
+              required: { value: true, message: "Preencha com sua senha." },
+              minLength: { value: 6, message: "Minimo de 6 caracteres." },
+              maxLength: { value: 20, message: "Maximo de 20 caracters." },
+            })}
+            type="password"
+            placeholder="Digite sua senha"
+          />
+          <small className="text-danger">{errors.senha?.message}</small>
+        </Form.Group>
+
+        <Button type="submit">Cadastrar</Button>
+      </Form>
+    </Container>
   );
 }
