45562beb5f8b7e8482b10381428c9741ae9659a8
gab-braga <gabrielbraga0712@gmail.com>
2025-02-12 13:52:53 -0300

    aula 24

--- src/pages/Calendario/Calendario.jsx
+++ src/pages/Calendario/Calendario.jsx
@@ -1,13 +1,23 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 export default function Calendario() {
   const [dadosIBGE, setDadosIBGE] = useState([]);
+  const { handleSubmit, register } = useForm();
 
   async function buscarDados() {
-    const endpoint = "https://servicodados.ibge.gov.br/api/v3/calendario";
+    const endpoint = "https://servicodados.ibge.gov.br/api/v3/calendario?qtd=20&de=2025-08-01&ate=2025-09-01";
+    const resposta = await axios.get(endpoint);
+    const json = resposta.data;
+    setDadosIBGE(json.items);
+  }
+
+  async function buscarDadosPesquisa(dados) {
+    const { id } = dados;
+    const endpoint = "https://servicodados.ibge.gov.br/api/v3/calendario/" + id;
     const resposta = await axios.get(endpoint);
     const json = resposta.data;
     setDadosIBGE(json.items);
@@ -22,6 +32,15 @@ export default function Calendario() {
       <h1>Calendário Governo</h1>
       <Link to="/">Home</Link>
 
+      <Form onSubmit={handleSubmit(buscarDadosPesquisa)} className="my-5">
+        <Form.Group className="mb-3" controlId="pesquisa">
+          <Form.Label>Digite o ID da Pesquisa</Form.Label>
+          <Form.Control {...register("id")} type="text" />
+        </Form.Group>
+
+        <Button type="submit">Procurar</Button>
+      </Form>
+
       <table className="w-100">
         <thead>
           <tr>
@@ -32,7 +51,7 @@ export default function Calendario() {
         </thead>
         <tbody>
           {dadosIBGE.map(dado => (
-            <tr key={dado.id}>
+            <tr key={dado.id} id={dado.produto_id}>
               <td className="border-2 p-2">{dado.titulo}</td>
               <td className="border-2 p-2">{dado.data_divulgacao}</td>
               <td className="border-2 p-2">{dado.descricao_produto}</td>
